eb1c3638be944db1226765a468884c27ecb9e8d0
Nicolas <nszchagas@gmail.com>
2024-06-29 17:39:01 -0300

    Ciclo 1 green state

--- trab1/src/main/java/br/unb/CadastroDeCliente.java
+++ trab1/src/main/java/br/unb/CadastroDeCliente.java
@@ -7,9 +7,19 @@ import java.util.Locale;
 public class CadastroDeCliente{
     public Cliente cadastraCliente(String nome, String regiao, String estado, String categoria) {
         List<String> categoriasValidas = List.of("padrao", "especial", "prime");
+         List<String> estadosValidos = List.of(
+            "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA",
+            "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN",
+            "RS", "RO", "RR", "SC", "SP", "SE", "TO"
+         );
+
         categoria = categoria.toLowerCase().trim();
+
         if (! categoriasValidas.contains(categoria) ) {
             throw new IllegalArgumentException(String.format("Categoria inválida: %s.", categoria));
+        }
+        if (! estadosValidos.contains(estado)) {
+            throw new IllegalArgumentException(String.format("Estado inválido: %s.", estado));
         }
 
 
