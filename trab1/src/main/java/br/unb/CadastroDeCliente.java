c80b41ef803f7f11f250cf52bc48e023e3f85fd2
Nicolas <nszchagas@gmail.com>
2024-06-29 17:07:54 -0300

    Ciclo 1 refatoração

--- trab1/src/main/java/br/unb/CadastroDeCliente.java
+++ trab1/src/main/java/br/unb/CadastroDeCliente.java
@@ -1,9 +1,13 @@
-package br.unb; 
+package br.unb;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CadastroDeCliente{
     public void cadastraCliente(String nome, String regiao, String estado, String categoria) {
-        // Categorias validas: padrao, especial, prime 
-        if (! (categoria.equals("padrao") || categoria.equals("especial") || categoria.equals("prime"))) {
+        List<String> categoriasValidas = List.of("padrao", "especial", "prime");
+
+        if (! categoriasValidas.contains(categoria) ) {
             throw new IllegalArgumentException(String.format("Categoria inválida: %s.", categoria));
         }
     }
