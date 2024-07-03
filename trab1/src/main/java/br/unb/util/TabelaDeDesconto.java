a3b0da1f5947331df427f193e55d6a84e948297a
Nicolas <nszchagas@gmail.com>
2024-07-03 01:25:29 -0300

    Desconto

--- trab1/src/main/java/br/unb/util/TabelaDeDesconto.java
+++ trab1/src/main/java/br/unb/util/TabelaDeDesconto.java
@@ -2,10 +2,16 @@ package br.unb.util;
 
 import br.unb.model.categorias.CategoriaDeCliente;
 
+import static br.unb.model.categorias.CategoriaDeCliente.ESPECIAL;
+
 public class TabelaDeDesconto {
 
+    public static double aplicaDesconto(double valorGasto, double frete, CategoriaDeCliente categoriaDeCliente, String metodoDePagamento){
+        if (categoriaDeCliente != ESPECIAL)
+            return 0;
+        if (! metodoDePagamento.equals("CARTAO_LOJA"))
+            return 0.1 * (valorGasto);
+        return 0.1 * valorGasto + 0.1 * (0.9 * valorGasto + frete);
 
-    public static double aplicaDesconto(double valorGasto, CategoriaDeCliente categoriaDeCliente, String metodoDePagamento){
-        return 0;
     }
 }
